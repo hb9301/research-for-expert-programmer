64e5383c0e2271cb15b7079c70761b646796bd7b
hbhyeon <hbhyeon@datastreams.co.kr>
2021-04-02 10:18:54 +0900

    Spring project로 변경  - spring_version = '5.2.12.RELEASE'  - spring_boot_version = '2.3.8.RELEASE'

---
+++ README.md
@@ -0,0 +1,5 @@
+# research-for-expert-programmer
+개발자 다운 개발자가 되기 위한 Research
+ - 문제풀이
+ - 코드리뷰
+ - 효율성 검사
---
+++ starter/src/test/java/org/research/starter/ResearchApplicationTests.java
@@ -0,0 +1,15 @@
+package org.research.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
