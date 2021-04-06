069a1b1d1856ba3dc67b1a33cf037ff18ec4f878
jinseob <bbb1125@naver.com>
2021-04-07 00:10:04 +0900

    2회차 메인문제

---
+++ java/com/study/lowlevel/jinseob/ArraySolution2BJS.java
@@ -0,0 +1,28 @@
+package com.study.lowlevel.jinseob;
+
+import java.util.Stack;
+
+public class ArraySolution2BJS {
+
+
+    public int solution(int[][] board, int[] moves){
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+
+        for(int move : moves){
+            for(int i = 0; i < board.length; i++){
+                if(board[i][move - 1] != 0){ // 인형을 잡는다.
+                    if(stack.size() != 0 && stack.peek() == board[i][move - 1]){ // 같은 아이템
+                        stack.pop(); // 마지막 아이템 제거
+                        result ++;
+                    }else{ //다른 아이템
+                        stack.push(board[i][move - 1]);
+                        board[i][move - 1] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        return result * 2;
+    }
+}
