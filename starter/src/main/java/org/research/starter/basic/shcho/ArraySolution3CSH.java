440184e260bca6d3e3aedae2a767d80f84fa0da9
shocho <csh628@gmail.com>
2021-04-12 02:56:11 +0900

    refs #3-2 - CSH

--- starter/src/main/java/org/research/starter/basic/shcho/ArraySolution3CSH.java
+++ starter/src/main/java/org/research/starter/basic/shcho/ArraySolution3CSH.java
@@ -1,11 +1,12 @@
 package org.research.starter.basic.shcho;
 
 import java.util.*;
+import java.util.function.*;
 
 public class ArraySolution3CSH {
     /**
-     * Second Programming - Array #3
-     * https://github.com/hb9301/research-for-expert-programmer/issues/3
+     * Second Programming - Array #3-1
+     * https://github.com/hb9301/research-for-expert-programmer/issues/4
      *
      * @param strings
      * @param n
@@ -27,4 +28,38 @@ public class ArraySolution3CSH {
         });
         return list.toArray(new String[0]);
     }
+
+    /**
+     * Second Programming - Array #3-2
+     * https://github.com/hb9301/research-for-expert-programmer/issues/4
+     *
+     * @param nums
+     * @return
+     */
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        int n = nums.length / 2;
+        ArrayList<Integer> list = new ArrayList();
+        for(int i : nums){
+            list.add(i);
+        }
+
+        while(n>0){
+            if(list.size() == 0) break;
+            list.removeIf(new Predicate<Integer>() {
+                @Override
+                public boolean test(Integer integer) {
+                    return (integer.equals(list.get(0)));
+                }
+            });
+            answer++;
+            n--;
+        }
+
+        return answer;
+    }
+
+
 }
